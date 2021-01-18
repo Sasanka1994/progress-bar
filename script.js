a7a0c6969ab1a23e957cf0856f7ec81239c79b70
Revin Sasanka <revinsasanka@Revins-MBP.lan>
2021-01-18 13:57:53 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+// to get all ciecles as node list (like an array) from .circle class
+const circles = document.querySelectorAll('.circle')
+
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    // to stop the incrementing onece it come to the end (4)
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    // console.log(currentActive)
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    // to stop the decrementing onece it come to the end (1)
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    // console.log(currentActive)
+
+    update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    // to get all active properties
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length 
+    - 1) * 100 + '%'
+
+    // console.log(progress.style.width)
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+
+}
